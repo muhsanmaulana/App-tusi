b1eaea4f20aa2b3486cec246e452fa9663f7bf1d
Rachel Siregar <50993102+acheell@users.noreply.github.com>
2019-11-17 19:50:23 +0700

    achel baru

---
+++ app/src/main/java/co/app/tusi/PilihanRute2MasukActivity.java
@@ -0,0 +1,14 @@
+package co.app.tusi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihanRute2MasukActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilihan_rute2_masuk);
+    }
+}
