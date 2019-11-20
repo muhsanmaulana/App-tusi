2322c286ca6aa4b89f6b7893b113bff01dcb9c49
Rachel Siregar <50993102+acheell@users.noreply.github.com>
2019-11-20 18:49:37 +0700

    layout slide, topup berubah dan nambah rute up umn, rute asrama ui

---
+++ app/src/main/java/co/app/tusi/PilRuteUpUmnActivity.java
@@ -0,0 +1,14 @@
+package co.app.tusi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilRuteUpUmnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pil_rute_up_umn);
+    }
+}
