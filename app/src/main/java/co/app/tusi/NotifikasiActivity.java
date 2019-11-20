c2b5ad21a568cd1a4df50b544ff4974d9ffb599c
muhsanmaulana <muhsan.maulana@gmail.com>
2019-11-21 05:08:03 +0700

    semua navigasi utama dah terhubung

--- app/src/main/java/co/app/tusi/NotifikasiActivity.java
+++ app/src/main/java/co/app/tusi/NotifikasiActivity.java
@@ -1,8 +1,14 @@
 package co.app.tusi;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotifikasiActivity extends AppCompatActivity {
 
@@ -11,6 +17,51 @@ public class NotifikasiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifikasi);
+
+        CircleImageView btnProfile = (CircleImageView) findViewById(R.id.profile_nav);
+        btnProfile.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(NotifikasiActivity.this,ProfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnTransactionNav = (ImageButton) findViewById(R.id.transaction_nav);
+        btnTransactionNav.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(NotifikasiActivity.this,TransaksiActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnNotifnav = (ImageButton) findViewById(R.id.notif_nav);
+        btnNotifnav.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(NotifikasiActivity.this,NotifikasiActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnBeranda = (ImageButton) findViewById(R.id.home_nav);
+        btnBeranda.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(NotifikasiActivity.this,BerandaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnGO = (ImageButton) findViewById(R.id.go_nav);
+        btnGO.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(NotifikasiActivity.this,PesanActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
--- app/src/main/java/co/app/tusi/TransaksiActivity.java
+++ app/src/main/java/co/app/tusi/TransaksiActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class TransaksiActivity extends AppCompatActivity {
 
@@ -19,6 +22,51 @@ public class TransaksiActivity extends AppCompatActivity {
 
             public void onClick(View v){
                 Intent i = new Intent(TransaksiActivity.this,PoinActivity.class);
+                startActivity(i);
+            }
+        });
+
+        CircleImageView btnProfile = (CircleImageView) findViewById(R.id.profile_nav);
+        btnProfile.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(TransaksiActivity.this,ProfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnTransactionNav = (ImageButton) findViewById(R.id.transaction_nav);
+        btnTransactionNav.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(TransaksiActivity.this,TransaksiActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnNotifnav = (ImageButton) findViewById(R.id.notif_nav);
+        btnNotifnav.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(TransaksiActivity.this,NotifikasiActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnBeranda = (ImageButton) findViewById(R.id.home_nav);
+        btnBeranda.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(TransaksiActivity.this,BerandaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton btnGO = (ImageButton) findViewById(R.id.go_nav);
+        btnGO.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent i = new Intent(TransaksiActivity.this,PesanActivity.class);
                 startActivity(i);
             }
         });
